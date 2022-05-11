fc28f20ccd9227a618a3fda84e85ea790307c611
BattleToaster26 <zcbaltzer@gmail.com>
2022-05-11 13:11:40 -0500

    works but has no game

--- app/src/main/java/com/example/steptracker/MainActivity.kt
+++ app/src/main/java/com/example/steptracker/MainActivity.kt
@@ -1,28 +1,42 @@
 package com.example.steptracker
 
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+import android.util.Log
+import android.view.View
+import android.widget.*
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
+
+
+    lateinit var startButton: Button
+
 
     private var sensorManager: SensorManager? = null
 
     private var running = false
 
-    private var totalSteps = 0f
+    private var totalSteps = 100f
 
     private var previousTotalSteps = 0f
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton = findViewById(R.id.startButton)
+
+
+        startButton.setOnClickListener() {
+
+        }
 
         loadData()
         resetSteps()
@@ -40,46 +54,65 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
             Toast.makeText(this, "No sensor detected on this device", Toast.LENGTH_SHORT).show()
         } else {
+
             sensorManager?.registerListener(this, stepSensor, SensorManager.SENSOR_DELAY_UI)
         }
-
-        override fun onSensorChanged(event: SensorEvent?) {
-
-            var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-
-            if (running) {
-                totalSteps = event!!.values[0]
-
-                val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
-
-                tv_stepsTaken.text = ("$currentSteps")
-            }
-        }
-
-
-        fun resetSteps() {
-            var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
-            tv_stepsTaken.setOnClickListener{
-
-                Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
-            }
-
-            tv_stepsTaken.setOnLongClickListener {
-                previousTotalSteps = totalSteps
-
-                tv_stepsTaken.text = 0.toString()
-
-                saveData()
-
-                true
-            }
-        }
-
-        private fun saveData() {
-            val sharedPreferences = getSharedPreferences("my Prefs", Context.MODE_PRIVATE)
-
-            val
-        }
-
     }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+
+        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+
+        if (running) {
+            totalSteps = event!!.values[0]
+
+            val currentSteps = totalSteps.toInt() - previousTotalSteps.toInt()
+
+            tv_stepsTaken.text = ("$currentSteps")
+        }
+    }
+
+    fun resetSteps() {
+        var tv_stepsTaken = findViewById<TextView>(R.id.tv_stepsTaken)
+        tv_stepsTaken.setOnClickListener {
+
+            Toast.makeText(this, "Long tap to reset steps", Toast.LENGTH_SHORT).show()
+        }
+
+        tv_stepsTaken.setOnLongClickListener {
+
+            previousTotalSteps = totalSteps
+
+            tv_stepsTaken.text = 0.toString()
+
+
+            saveData()
+
+            true
+        }
+    }
+
+    private fun saveData() {
+
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+
+        val editor = sharedPreferences.edit()
+        editor.putFloat("key1", previousTotalSteps)
+        editor.apply()
+    }
+
+    private fun loadData() {
+
+        val sharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val savedNumber = sharedPreferences.getFloat("key1", 0f)
+
+
+        Log.d("MainActivity", "$savedNumber")
+
+        previousTotalSteps = savedNumber
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
 }
